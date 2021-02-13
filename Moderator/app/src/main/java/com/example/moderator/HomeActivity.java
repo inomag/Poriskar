24b6ef9464fe70cf1a6c515214e2d5c67e4e0dae
Abhimanyu Giri <abhi852913@gmail.com>
2021-02-13 07:59:36 +0530

    homepage

--- Moderator/app/src/main/java/com/example/moderator/HomeActivity.java
+++ Moderator/app/src/main/java/com/example/moderator/HomeActivity.java
@@ -1,14 +1,61 @@
 package com.example.moderator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.widget.NestedScrollView;
+
+import com.google.android.material.tabs.TabLayout;
+
 
 public class HomeActivity extends AppCompatActivity {
+
+    private TabLayout tab_layout;
+    private NestedScrollView nested_scroll_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        initToolbar();
+        initComponent();
     }
+
+    private void initToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_menu);
+        toolbar.getNavigationIcon().setColorFilter(getResources().getColor(R.color.green), PorterDuff.Mode.SRC_ATOP);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(null);
+
+    }
+
+    private void initComponent() {
+        nested_scroll_view = (NestedScrollView) findViewById(R.id.nested_scroll_view);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.homeactivity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        } else {
+            Toast.makeText(getApplicationContext(), item.getTitle(), Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
