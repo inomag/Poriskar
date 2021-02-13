71593e51cb2e65dea2161b5a9bea165572c00a20
Inomag <pratiksr12345@gmail.com>
2021-02-13 09:05:58 +0530

    map marker

--- User/app/src/main/java/com/example/user/MapMarker.java
+++ User/app/src/main/java/com/example/user/MapMarker.java
@@ -1,35 +1,51 @@
 package com.example.user;
 
 
-
-
-
-
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class MapMarker extends AppCompatActivity implements OnMapReadyCallback{
+public class MapMarker extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener, GoogleMap.OnMapLongClickListener {
 
     private ArrayList<String> routes;
     private String[] arr;
@@ -40,6 +56,11 @@ public class MapMarker extends AppCompatActivity implements OnMapReadyCallback{
     private MaterialButton continue_to_home;
     private AutoCompleteTextView spinner;
     private TextInputLayout spinner_layout;
+    String id;
+    private Location mLastLocation;
+    private GoogleApiClient mGoogleApiClient;
+    private int ACCESS_LOCATION_REQUEST_CODE = 10001;
+    FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,13 +69,24 @@ public class MapMarker extends AppCompatActivity implements OnMapReadyCallback{
         setContentView(R.layout.activity_map_marker);
 
 
+        Intent intent = getIntent();
+
+        id = intent.getStringExtra("id");
+
+        // centerMapOnMyLocation();
+        //  onMapReady(mMap);
+
+
+        Toast.makeText(getApplicationContext(), id, Toast.LENGTH_SHORT).show();
+
 
         Window window = this.getWindow();
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-        window.setStatusBarColor(ContextCompat.getColor(this,R.color.black));
+        window.setStatusBarColor(ContextCompat.getColor(this, R.color.black));
 
-        spinner_layout = (TextInputLayout)findViewById(R.id.spinner_layout);
+
+        spinner_layout = (TextInputLayout) findViewById(R.id.spinner_layout);
 
         arr = getResources().getStringArray(R.array.route_names);
 
@@ -62,22 +94,23 @@ public class MapMarker extends AppCompatActivity implements OnMapReadyCallback{
         routes.addAll(Arrays.asList(arr));
 
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.route,routes);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.route, routes);
 
-        spinner = (AutoCompleteTextView)findViewById(R.id.spinner);
+        spinner = (AutoCompleteTextView) findViewById(R.id.spinner);
         spinner.setAdapter(adapter);
         spinner.setThreshold(1);
 
 
-        continue_to_home = (MaterialButton)findViewById(R.id.done_btn);
+        continue_to_home = (MaterialButton) findViewById(R.id.done_btn);
         continue_to_home.setOnClickListener(v -> {
 //            startActivity(new Intent(MapMarker.this, HomeActivity.class));
 //            finish();
         });
 
-        SupportMapFragment supportMapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         assert supportMapFragment != null;
         supportMapFragment.getMapAsync(this);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
     }
 
 
@@ -85,5 +118,132 @@ public class MapMarker extends AppCompatActivity implements OnMapReadyCallback{
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+//        mMap.setOnMapLongClickListener(this);
+////        mMap.setOnMarkerClickListener(this);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            enableUserLocation();
+            zoomToUserLocation();
+        } else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, ACCESS_LOCATION_REQUEST_CODE);
+
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, ACCESS_LOCATION_REQUEST_CODE);
+
+            }
+        }
+
+
+    }
+
+    private void zoomToUserLocation() {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+
+            Toast.makeText(this, "Network ISSUE", Toast.LENGTH_SHORT).show();
+            return;
+        } else {
+            LocationManager locationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+            boolean gps_enabled = false;
+            boolean network_enabled = false;
+
+            try {
+                gps_enabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+            } catch (Exception ex) {
+            }
+
+
+            if (!gps_enabled ) {
+                new AlertDialog.Builder(getApplicationContext()).setMessage(R.string.gps_network_not_enabled)
+                        .setPositiveButton(R.string.open_location_settings, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                getApplicationContext().startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                            }
+                        });
+
+
+            } else {
+                Task<Location> locationTask = fusedLocationProviderClient.getLastLocation();
+                locationTask.addOnSuccessListener(new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 12));
+                        mMap.addMarker(new MarkerOptions().position(latLng));
+                    }
+                });
+            }
+        }
+    }
+
+    private void enableUserLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this,"Unable to get Location",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else {
+
+            mMap.setMyLocationEnabled(true);
+        }
+
+
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        return false;
+    }
+
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+            LocationManager locationManager= (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+            boolean gps_enabled = false;
+            boolean network_enabled = false;
+
+            try{
+                gps_enabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+            }catch(Exception ex){}
+            try{
+                network_enabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+            }catch (Exception ex){}
+
+            if(!gps_enabled && !network_enabled){
+                new AlertDialog.Builder(getApplicationContext()).setMessage(R.string.gps_network_not_enabled)
+                        .setPositiveButton(R.string.open_location_settings, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                getApplicationContext().startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                            }
+                        }).setNegativeButton(R.string.Cancel,null).show();
+
+            }
+            else {
+                enableUserLocation();
+                zoomToUserLocation();
+            }
+        }
+        else
+        {
+
+        }
     }
 }
--- User/app/src/main/java/com/example/user/Signup.java
+++ User/app/src/main/java/com/example/user/Signup.java
@@ -4,10 +4,12 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -29,31 +31,37 @@ public class Signup extends AppCompatActivity {
     String user_name,user_pass,phone_no,id;
     Long user_phone;
 
-    private FirebaseFirestore db;
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+
         Window window = this.getWindow();
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         window.setStatusBarColor(ContextCompat.getColor(this,R.color.white));
 
         continue_to_map = (MaterialButton)findViewById(R.id.continue_to_map);
 
-        name = (TextInputEditText) findViewById(R.id.name_signup);
-        phoneNo = (TextInputEditText) findViewById(R.id.phone_signup);
-        password = (TextInputEditText) findViewById(R.id.pass_word_signup);
 
-
-        user_name = name.getText().toString();
-        user_pass = password.getText().toString();
-//            phone_no = phoneNo.getText().toString();
-//            user_phone = Long.parseLong(phone_no);
 
         continue_to_map.setOnClickListener(v -> {
-         //   addData();
-            startActivity(new Intent(Signup.this,MapMarker.class));
+
+            name = (TextInputEditText) findViewById(R.id.name_signup);
+            phoneNo = (TextInputEditText) findViewById(R.id.phone_signup);
+            password = (TextInputEditText) findViewById(R.id.pass_word_signup);
+
+
+            user_name = name.getText().toString();
+            user_pass = password.getText().toString();
+            phone_no = phoneNo.getText().toString();
+            user_phone = Long.parseLong(phone_no);
+            addData();
+
+
         });
 
         to_login = (MaterialButton)findViewById(R.id.back_to_login);
@@ -64,22 +72,27 @@ public class Signup extends AppCompatActivity {
 
     }
 
-//    private void addData() {
-//        Map<String, Object> user = new HashMap<>();
-//        user.put("name",user_name);
-//        user.put("phone_no",user_phone);
-//        user.put("password",user_pass);
-//
-//        db.collection("users").add(user).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
-//            @Override
-//            public void onComplete(@NonNull Task<DocumentReference> task) {
-//                if (task.isSuccessful()){
-//                    id = task.getResult().getId();
-//                    Toast.makeText(getApplicationContext(),id,Toast.LENGTH_SHORT).show();
-//                }
-//            }
-//        });
-//    }
+    private void addData() {
+        Map<String, Object> user = new HashMap<>();
+        user.put("name",user_name);
+        user.put("phone_no",user_phone);
+        user.put("password",user_pass);
+
+        db.collection("users").add(user).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentReference> task) {
+                if (task.isSuccessful()){
+                    id = task.getResult().getId();
+
+
+
+                    Intent intent = new Intent(getApplicationContext(),MapMarker.class);
+                    intent.putExtra("id",id);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
 
 
 }
