ceb36c7e9511b8dd3e1c6d08de5d273d30c3a324
Bucephalus <bhargabnath691@gmail.com>
2021-02-13 13:16:38 +0530

    drivers and routes pages are styled

--- Admin/routes/drivers.js
+++ Admin/routes/drivers.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { addDriver, getAllDrivers, updateDriver, assign_route_get} = require('../controller/drivers');
+const { addDriver, getAllDrivers, updateDriver, assign_route_get } = require('../controller/drivers');
 const { requireAuth } = require('../middleware');
 
 router.get('/drivers', requireAuth, getAllDrivers);
@@ -11,6 +11,6 @@ router.post('/drivers', requireAuth, addDriver);
 
 //assign route to driver
 router.get('/drivers/assign-route', requireAuth, assign_route_get);
-router.post('/drivers/assign-route',requireAuth, updateDriver);
+router.post('/drivers/assign-route', requireAuth, updateDriver);
 
 module.exports = router;
